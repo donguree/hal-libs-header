bd2751fd9937144a3cd79415ffe3dae19bdfed4c
Jeogeun Min <jeongeun.min@lge.com>
2017-03-03 09:39:49 +0900

    Fix hal_drm.h and hal_svp.h for common policy
    :Release Notes:
    Fix hal_drm.h and hal_svp.h for common policy
    
    :Detailed Notes:
    Fix variable type using defined at hal_common.h and
    remove unused functions
    
    :Testing Performed:
    NA
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [TMFTASK-9801] Fix hal_drm.h and hal_svp.h for integration of HAL
    
    Change-Id: I4d59cd3797618c25e20bd28c386c84109feb94dc

--- hal_inc/hal_svp.h
+++ hal_inc/hal_svp.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *   TV LABORATORY, LG ELECTRONICS INC., SEOUL, KOREA
- *   Copyright(c) 2013 by LG Electronics Inc.
+ *   Copyright(c) 2013-2017 by LG Electronics Inc.
  *
  *   All rights reserved. No part of this work may be reproduced, stored in a
  *   retrieval system, or transmitted by any means without prior written
@@ -45,54 +45,53 @@
     Type Definitions
 ******************************************************************************/
 typedef enum {
-	HAL_SVP_REFORMATTER_NONE			= 0,
-	HAL_SVP_REFORMATTER_AVC 			= 1,
-	HAL_SVP_REFORMATTER_HEVC 			= 2,
-	HAL_SVP_REFORMATTER_HEVC_COMPLETE	= 3,
+  HAL_SVP_REFORMATTER_NONE           = 0,
+  HAL_SVP_REFORMATTER_AVC            = 1,
+  HAL_SVP_REFORMATTER_HEVC           = 2,
+  HAL_SVP_REFORMATTER_HEVC_COMPLETE  = 3,
 } SVP_REFORMATTER_T;
 
 typedef enum {
-	HAL_SVP_ERROR	= -1,
-	HAL_SVP_OK		= 0,
-	HAL_SVP_NO_AU	= 1,
-	HAL_SVP_WAIT	= 2
+  HAL_SVP_ERROR	= -1,
+  HAL_SVP_OK    = 0,
+  HAL_SVP_NO_AU	= 1,
+  HAL_SVP_WAIT	= 2
 } SVP_STATUS_T;
 
 /******************************************************************************
     Function Declaration
 ******************************************************************************/
 
-SVP_STATUS_T HAL_SVP_OpenSecureCpb(unsigned int secCpbAddr, int secCpbSize);
-SVP_STATUS_T HAL_SVP_OpenSecureCpbEx(unsigned int secCpbAddr, int secCpbSize, int align);
+SVP_STATUS_T HAL_SVP_OpenSecureCpb(UINT32 secCpbAddr, SINT32 secCpbSize);
+SVP_STATUS_T HAL_SVP_OpenSecureCpbEx(UINT32 secCpbAddr, SINT32 secCpbSize, SINT32 align);
 SVP_STATUS_T HAL_SVP_CloseSecureCpb(void);
-SVP_STATUS_T HAL_SVP_CloseSecureCpbEx(unsigned int secCpbAddr, int secCpbSize);
+SVP_STATUS_T HAL_SVP_CloseSecureCpbEx(UINT32 secCpbAddr, SINT32 secCpbSize);
 BOOLEAN HAL_SVP_IsSecureReformatterSupported(void);
 SVP_STATUS_T HAL_SVP_SetSecureReformatter(SVP_REFORMATTER_T reformatterType);
-SVP_STATUS_T HAL_SVP_SetSecureReformatterEx(unsigned int secCpbAddr, SVP_REFORMATTER_T reformatterType);
-SVP_STATUS_T HAL_SVP_GetSecureCpbInformation (unsigned long vdecHandle,
-	unsigned long *pSecCpbAddr, int *pSecCpbSize, int *pReadOffset, int *pWriteOffset);
-SVP_STATUS_T HAL_SVP_GetSecureDecryptBufferHandle(unsigned int *pSecDecBufHandle, unsigned int *pSecDecBufSize);
-SVP_STATUS_T HAL_SVP_GetSecureDecryptBufferHandleEx(unsigned int secCpbAddr, unsigned int *pSecDecBufHandle, unsigned int *pSecDecBufSize);
-SVP_STATUS_T HAL_SVP_LockSecureDecryptBufferHandle(void);
-SVP_STATUS_T HAL_SVP_UnlockSecureDecryptBufferHandle(void);
-SVP_STATUS_T HAL_SVP_GetAuInformation(unsigned int *pAuWritePoint, int *pAuWriteLength,
-	unsigned int *pWritePoint, unsigned int readPoint, unsigned int secDecBufHandle, unsigned int srcLength);
-SVP_STATUS_T HAL_SVP_CheckAuTypeIsDolbyHdrMeta(unsigned int auWritePoint, int auWriteLength, BOOLEAN *pIsMetaType);
+SVP_STATUS_T HAL_SVP_SetSecureReformatterEx(UINT32 secCpbAddr, SVP_REFORMATTER_T reformatterType);
+SVP_STATUS_T HAL_SVP_GetSecureCpbInformation (ULONG vdecHandle,
+	ULONG *pSecCpbAddr, SINT32 *pSecCpbSize, SINT32 *pReadOffset, SINT32 *pWriteOffset);
+SVP_STATUS_T HAL_SVP_GetSecureDecryptBufferHandle(UINT32 *pSecDecBufHandle, UINT32 *pSecDecBufSize);
+SVP_STATUS_T HAL_SVP_GetSecureDecryptBufferHandleEx(UINT32 secCpbAddr, UINT32 *pSecDecBufHandle, UINT32 *pSecDecBufSize);
+SVP_STATUS_T HAL_SVP_GetAuInformation(UINT32 *pAuWritePoSINT32, SINT32 *pAuWriteLength,
+	UINT32 *pWritePoSINT32, UINT32 readPoSINT32, UINT32 secDecBufHandle, UINT32 srcLength);
+SVP_STATUS_T HAL_SVP_CheckAuTypeIsDolbyHdrMeta(UINT32 auWritePoSINT32, SINT32 auWriteLength, BOOLEAN *pIsMetaType);
 SVP_STATUS_T HAL_SVP_FlushSecureDecryptBuffer(void);
-SVP_STATUS_T HAL_SVP_FlushSecureDecryptBufferEx(unsigned int secCpbAddr);
+SVP_STATUS_T HAL_SVP_FlushSecureDecryptBufferEx(UINT32 secCpbAddr);
 
-SVP_STATUS_T HAL_SVP_Widevine_DecryptVideo(unsigned char *pIv, unsigned char *pSrc, unsigned int srcLength,
-	unsigned int secDecBufHandle, unsigned int secDecBufwriteOffset, unsigned int *pOutputLength);
-SVP_STATUS_T HAL_SVP_Widevine_DecryptAudio(unsigned char *pIv, unsigned char *pSrc, unsigned int srcLength,
-	unsigned char *pOutput, unsigned int *pOutputLength);
+SVP_STATUS_T HAL_SVP_Widevine_DecryptVideo(UNIT8 *pIv, UNIT8 *pSrc, UINT32 srcLength,
+	UINT32 secDecBufHandle, UINT32 secDecBufwriteOffset, UINT32 *pOutputLength);
+SVP_STATUS_T HAL_SVP_Widevine_DecryptAudio(UNIT8 *pIv, UNIT8 *pSrc, UINT32 srcLength,
+	UNIT8 *pOutput, UINT32 *pOutputLength);
 
-SVP_STATUS_T HAL_SVP_WidevineSelectKey(unsigned int session, const unsigned char *key_id, size_t key_id_length);
-SVP_STATUS_T HAL_SVP_WidevineDecryptCTR(unsigned int session, const unsigned char *data_addr, size_t data_length, char is_encrypted,
-	const unsigned char *iv, size_t block_offset, const void *out_buffer, unsigned char subsample_flags);
+SVP_STATUS_T HAL_SVP_WidevineSelectKey(UINT32 session, const UNIT8 *key_id, size_t key_id_length);
+SVP_STATUS_T HAL_SVP_WidevineDecryptCTR(UINT32 session, const UNIT8 *data_addr, size_t data_length, SINT8 is_encrypted,
+	const UNIT8 *iv, size_t block_offset, const void *out_buffer, UNIT8 subsample_flags);
 
-SVP_STATUS_T HAL_SVP_CopyToSecureDecryptBuffer(unsigned int secDecBufHandle, unsigned int secDecBufwriteOffset,
-	unsigned char *pSrc, int length);
-SVP_STATUS_T HAL_SVP_CopyFromSecureBuffer(unsigned int srcHandle, unsigned char *pOutput, int length);
+SVP_STATUS_T HAL_SVP_CopyToSecureDecryptBuffer(UINT32 secDecBufHandle, UINT32 secDecBufwriteOffset,
+	UNIT8 *pSrc, SINT32 length);
+
+SVP_STATUS_T HAL_SVP_CopyFromSecureBuffer(UINT32 srcHandle, UNIT8 *pOutput, SINT32 length);
 
 #endif /* _HAL_SVP_H_ */
 
