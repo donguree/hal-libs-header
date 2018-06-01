b874bf7a5ac5953b9a1c75010f8fbccfbdbd0afb
Sunwoong Yoo <sunwoong.yoo@lge.com>
2018-06-01 11:26:10 +0900

    Add New HAL Header for CI+ ECP
    :Release Notes:
    Add HAL_ECP APIs
    
    : Detailed Notes:
    target branch [master]
    HAL IG Update [yes]
    Request to All SoC [yes]
    
    Add below HAL_ECP APIs
    HAL_ECP_CA_CI_PLUS_ExecuteTEE
    HAL_ECP_CI_PLUS_LoadKeyMaterials
    HAL_ECP_CI_PLUS_LoadCertKey
    HAL_ECP_CI_PLUS_WriteCertKey
    HAL_ECP_CI_PLUS_SetCryptographyByChannel
    HAL_ECP_CI_PLUS_SetCipherKeysByChannel
    
    :Issues Addressed:
    [SCDCR-1619]
    [RTKREQ-381]
    [MSTARREQ-541]
    [MSIXTEEN-675]
    [KTASKWBS-7898]
    [MTASKWBS-74294]
    
    Change-Id: I5b07ad8e26dafae116d9a8b48228675cb0ee1ec7

---
+++ hal_inc/hal_ecp.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+ *   TV LABORATORY, LG ELECTRONICS INC., SEOUL, KOREA
+ *   Copyright(c) 2018 by LG Electronics Inc.
+ *
+ *   All rights reserved. No part of this work may be reproduced, stored in a
+ *   retrieval system, or transmitted by any means without prior written
+ *   permission of LG Electronics Inc.
+ *****************************************************************************/
+
+/** @file hal_ecp.h
+ *
+ * This header file defines the HAL functions related to CI+ ECP
+ *
+ *  @author     Sunwoong Yoo (sunwoong.yoo@lge.com)
+ *  @version    1.0
+ *  @date       2018.05.30
+ *  @date
+ *  @note	    Additional information.
+ *  @see
+ */
+
+/******************************************************************************
+    Header File Guarder
+******************************************************************************/
+#ifndef	_HAL_ECP_H_
+#define	_HAL_ECP_H_
+
+/******************************************************************************
+    Control Constants
+******************************************************************************/
+
+/******************************************************************************
+    File Inclusions
+******************************************************************************/
+#include "hal_common.h"
+#include "hal_ci.h"
+
+/******************************************************************************
+    Constant Definitions
+******************************************************************************/
+
+/******************************************************************************
+    Macro Definitions
+******************************************************************************/
+
+/******************************************************************************
+    Type Definitions
+******************************************************************************/
+
+/******************************************************************************
+    Function Declaration
+******************************************************************************/
+int HAL_ECP_CA_CI_PLUS_ExecuteTEE(int func_id, void *ptr, int size);
+DTV_STATUS_T HAL_ECP_CI_PLUS_LoadKeyMaterials(void);
+DTV_STATUS_T HAL_ECP_CI_PLUS_LoadCertKey(void);
+DTV_STATUS_T HAL_ECP_CI_PLUS_WriteCertKey(UINT8 *pData , int dataSize);
+DTV_STATUS_T HAL_ECP_CI_PLUS_SetCryptographyByChannel(SINT32 deviceHandle, CIPLUS_DECRYPT_KEY_DST_T eDecryptKeyDst, CIPLUS_CRYPTOGRAPHY_T eCryptography);
+DTV_STATUS_T HAL_ECP_CI_PLUS_SetCipherKeysByChannel(SINT32 deviceHandle, CIPLUS_DECRYPT_KEY_DST_T eDecryptKeyDst, CIPLUS_CRYPTOGRAPHY_T eCryptography, CIPLUS_CIPHER_KEY_T eKey);
+
+#endif /* _HAL_ECP_H_ */
