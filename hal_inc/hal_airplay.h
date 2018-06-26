0358f508de4eecc37e2a865affa073e95fb1dc44
Kyungnam <kyungnam.bae@lge.com>
2018-06-26 13:10:38 +0900

    AIRPLAY : Add Key RW API
    :Release Notes:
    add functions below
     - HAL_AIRPLAY_MFi_WriteKeySet
     - HAL_AIRPLAY_MFi_VerifyKeySet
     - HAL_AIRPLAY_MFi_GetKeyInfo
    
    : Detailed Notes:
    target branch [master]
    HAL IG Update [yes]
    Request to All SoC [yes]
    
    To write, read and verify, define the new hals.
    See for HAL IG - http://collab.lge.com/main/display/NCDRNTF/Airplay
    
    :Issues Addressed:
    [SCDCR-1710]
    [TVPLAT-14686]
    [MSTARREQ-587]
    [RTKREQ-429]
    
    Change-Id: I83056ce009c3c12dda0d67e799cd1c7d7b50acb9

---
+++ hal_inc/hal_airplay.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *   DTV LABORATORY, LG ELECTRONICS INC., SEOUL, KOREA
+ *   Copyright(c) 2018 by LG Electronics Inc.
+ *
+ *   All rights reserved. No part of this work may be reproduced, stored in a
+ *   retrieval system, or transmitted by any means without prior written
+ *   permission of LG Electronics Inc.
+ *****************************************************************************/
+
+/** @file 		hal_airplay.h
+ *
+ *  header of Common Interface HAL Fuction.
+ *
+ *  @author		kyungnam.bae
+ *  @version	1.0
+ *  @date		2018.6.26
+ *  @date
+ *  @note		Additional information.
+ *  @see
+ */
+
+/******************************************************************************
+	Header File Guarder
+******************************************************************************/
+#ifndef _COMMON_HAL_AIRPLAY_H_
+#define _COMMON_HAL_AIRPLAY_H_
+
+/******************************************************************************
+	Control Constants
+******************************************************************************/
+
+/******************************************************************************
+	File Inclusions
+******************************************************************************/
+#include "hal_common.h"
+
+/******************************************************************************
+	Constant Definitions
+******************************************************************************/
+
+/******************************************************************************
+	Macro Definitions
+******************************************************************************/
+
+/******************************************************************************
+	Type Definitions
+******************************************************************************/
+
+/******************************************************************************
+	Function Declaration
+******************************************************************************/
+int HAL_AIRPLAY_MFi_WriteKeySet(unsigned int argc, void **argv);
+int HAL_AIRPLAY_MFi_VerifyKeySet(void);
+int HAL_AIRPLAY_MFi_GetKeyInfo(unsigned char **out_org_key_file_name,
+		unsigned char **out_index, unsigned char **out_mfi_obj_id,
+		unsigned char **out_ppid);
+
+#endif				/* _COMMON_HAL_AIRPLAY_H_ */
