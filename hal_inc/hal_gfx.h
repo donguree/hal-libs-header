49f99155de85482532c783e0994c2372a9a1cce9
Jinho Lee <jh0506.lee@lge.com>
2019-02-25 15:34:14 +0900

    Graphic : Add hal_gfx module
    :Release Notes:
    Add hal_gfx.h file
    
    :Detailed Notes:
    target branch [master]
    HAL IG Update [yes]
    Request to All Soc [yes]
    The GFX is 2D Graphic Device to provide 2D graphic operations.
    
    :Issues Addressed:
    [TVPLAT-27580] Implement:Implement the SoC backend (mali, gfx) - SIC
    [SCDCR-2202] [SIC]
    [MSTARREQ-921] [MStar]
    [RTKREQ-669] [RTK]
    
    Change-Id: I46b852043400cef14b8bc7820b39c0f3f7802ef7

---
+++ hal_inc/hal_gfx.h
@@ -0,0 +1,75 @@
+/******************************************************************************
+ *	 DTV LABORATORY, LG ELECTRONICS INC., SEOUL, KOREA
+ *	 Copyright(c) 2019 by LG Electronics Inc.
+ *
+ *	 All rights reserved. No part of this work may be reproduced, stored in a
+ *	 retrieval system, or transmitted by any means without prior written
+ *	 permission of LG Electronics Inc.
+ *****************************************************************************/
+
+#ifndef _HAL_GFX_H_
+#define _HAL_GFX_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef hal_gfx_ptr
+typedef	void *__hal_gfx_ptr;
+#define hal_gfx_ptr __hal_gfx_ptr
+#endif
+
+#ifndef hal_gfx_int
+typedef	int __hal_gfx_int;
+#define hal_gfx_int __hal_gfx_int
+#endif
+
+#ifndef hal_gfx_int32
+typedef	signed int __hal_gfx_int32;
+#define hal_gfx_int32 __hal_gfx_int32
+#endif
+
+typedef enum {
+    Surface_Format_Unknown = 0,
+    Surface_Format_A8R8G8B8,
+    Surface_Format_FBC,
+} hal_gfx_surface_format;
+
+typedef struct {
+    hal_gfx_int32 x;
+    hal_gfx_int32 y;
+    hal_gfx_int32 width;
+    hal_gfx_int32 height;
+} hal_gfx_rect;
+
+typedef struct {
+    hal_gfx_surface_format format;
+    hal_gfx_int32 width;
+    hal_gfx_int32 height;
+    hal_gfx_int32 stride;
+} hal_gfx_surface_attr;
+
+typedef struct {
+    hal_gfx_surface_attr attribute;
+    hal_gfx_int fd;
+    hal_gfx_ptr handle;
+} hal_gfx_surface;
+
+hal_gfx_int
+hal_gfx_import_surface_by_fd(hal_gfx_int fd,
+                             hal_gfx_surface_attr attr,
+                             hal_gfx_ptr *handle);
+
+hal_gfx_int
+hal_gfx_release_surface(hal_gfx_ptr handle);
+
+hal_gfx_int
+hal_gfx_blit(hal_gfx_surface dst,
+             hal_gfx_surface *srcs,
+             hal_gfx_int src_len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _HAL_GFX_H_ */
