96db8293472c0b45c46baeb822f0b0277ffbb2d3
Hyung Song <hyung.song@lge.com>
2022-05-24 17:21:08 +0900

    Update VO/JPEG/PNG document
    :Detailed Notes:
    Update VO/JPEG/PNG document
    
    :Testing Performed:
    NA
    
    :Issues Addressed:
    [TVPLAT-191867]
    
    Change-Id: Id0b4a2d2ebb33422db020bac2f3d6def235d5dfa

--- hal_inc/hal_jpeg.h
+++ hal_inc/hal_jpeg.h
@@ -25,61 +25,61 @@
  * @brief This enumeration describes the supported pixel formats.
  **/
 typedef enum {
-    HAL_JPEG_PIXEL_FORMAT_NONE = 0, /* none of these */
-    HAL_JPEG_PIXEL_FORMAT_PALETTE,  /* palette color type */
-    HAL_JPEG_PIXEL_FORMAT_GRAYSCALE,    /* 8bit gray scale */
-    HAL_JPEG_PIXEL_FORMAT_RGB,  /* 24bit RGB */
-    HAL_JPEG_PIXEL_FORMAT_BGR,  /* 24bit BGR */
-    HAL_JPEG_PIXEL_FORMAT_ARGB, /* 32bit ARGB */
-    HAL_JPEG_PIXEL_FORMAT_ABGR, /* 32bit ABGR */
-    HAL_JPEG_PIXEL_FORMAT_YUV444P,  /* planar format with each U/V values plane (YYYY UUUU VVVV) */
-    HAL_JPEG_PIXEL_FORMAT_YUV444I,  /* planar format with interleaved U/V values (YYYY UVUVUVUV) */
-    HAL_JPEG_PIXEL_FORMAT_YUV422P,  /* semi-planar format with each U/V values plane (2x1 subsampling ; YYYY UU VV) */
-    HAL_JPEG_PIXEL_FORMAT_YUV422I,  /* semi-planar format with interleaved U/V values (2x1 subsampling ; YYYY UVUV) */
-    HAL_JPEG_PIXEL_FORMAT_YUV422YUYV,   /* interleaved YUV values (Y0U0Y0V0Y1U1Y1V1) for MStar Chip Vender */
-    HAL_JPEG_PIXEL_FORMAT_YUV420P,  /* semi-planar format with each U/V values plane (2x2 subsampling ; YYYYYYYY UU VV) */
-    HAL_JPEG_PIXEL_FORMAT_YUV420I,  /* semi-planar format with interleaved U/V values (2x2 subsampling ; YYYYYYYY UVUV) */
-    HAL_JPEG_PIXEL_FORMAT_YUV400,   /* 8bit Y plane without U/V values */
-    HAL_JPEG_PIXEL_FORMAT_YUV224P,  /* semi-planar format with each U/V values plane, 1 Ysamples has 2 U/V samples to horizontal (Y0Y1 U0U0U1U1V0V0V1V1) */
-    HAL_JPEG_PIXEL_FORMAT_YUV224I,  /* semi-planar format with interleaved U/V values (Y0Y1 U0V0U0V0U1V1U1V1) */
-    HAL_JPEG_PIXEL_FORMAT_YUV444SP, /* sequential packed with non-planar (YUVYUVYUV...) */
-    HAL_JPEG_PIXEL_FORMAT_ALPHAGRAYSCALE,   /* gray scale with alpha */
-    HAL_JPEG_PIXEL_FORMAT_MAX,  /* maximum number of HAL_JPEG_PIXEL_FORMAT */
+    HAL_JPEG_PIXEL_FORMAT_NONE = 0, /**< none of these */
+    HAL_JPEG_PIXEL_FORMAT_PALETTE,  /**< palette color type */
+    HAL_JPEG_PIXEL_FORMAT_GRAYSCALE,    /**< 8bit gray scale */
+    HAL_JPEG_PIXEL_FORMAT_RGB,  /**< 24bit RGB */
+    HAL_JPEG_PIXEL_FORMAT_BGR,  /**< 24bit BGR */
+    HAL_JPEG_PIXEL_FORMAT_ARGB, /**< 32bit ARGB */
+    HAL_JPEG_PIXEL_FORMAT_ABGR, /**< 32bit ABGR */
+    HAL_JPEG_PIXEL_FORMAT_YUV444P,  /**< planar format with each U/V values plane (YYYY UUUU VVVV) */
+    HAL_JPEG_PIXEL_FORMAT_YUV444I,  /**< planar format with interleaved U/V values (YYYY UVUVUVUV) */
+    HAL_JPEG_PIXEL_FORMAT_YUV422P,  /**< semi-planar format with each U/V values plane (2x1 subsampling ; YYYY UU VV) */
+    HAL_JPEG_PIXEL_FORMAT_YUV422I,  /**< semi-planar format with interleaved U/V values (2x1 subsampling ; YYYY UVUV) */
+    HAL_JPEG_PIXEL_FORMAT_YUV422YUYV,   /**< interleaved YUV values (Y0U0Y0V0Y1U1Y1V1) for MStar Chip Vender */
+    HAL_JPEG_PIXEL_FORMAT_YUV420P,  /**< semi-planar format with each U/V values plane (2x2 subsampling ; YYYYYYYY UU VV) */
+    HAL_JPEG_PIXEL_FORMAT_YUV420I,  /**< semi-planar format with interleaved U/V values (2x2 subsampling ; YYYYYYYY UVUV) */
+    HAL_JPEG_PIXEL_FORMAT_YUV400,   /**< 8bit Y plane without U/V values */
+    HAL_JPEG_PIXEL_FORMAT_YUV224P,  /**< semi-planar format with each U/V values plane, 1 Ysamples has 2 U/V samples to horizontal (Y0Y1 U0U0U1U1V0V0V1V1) */
+    HAL_JPEG_PIXEL_FORMAT_YUV224I,  /**< semi-planar format with interleaved U/V values (Y0Y1 U0V0U0V0U1V1U1V1) */
+    HAL_JPEG_PIXEL_FORMAT_YUV444SP, /**< sequential packed with non-planar (YUVYUVYUV...) */
+    HAL_JPEG_PIXEL_FORMAT_ALPHAGRAYSCALE,   /**< gray scale with alpha */
+    HAL_JPEG_PIXEL_FORMAT_MAX,  /**< maximum number of HAL_JPEG_PIXEL_FORMAT */
 } HAL_JPEG_PIXEL_FORMAT;
 
 /**
  * @brief This enumeration describes down-sampling factor using HW decoder.
  **/
 typedef enum {
-    HAL_JPEG_DECODE_OPTION_SCALING_NONE = 0,    /* none of these */
-    HAL_JPEG_DECODE_OPTION_SCALING_1_1, /* 1/1 */
-    HAL_JPEG_DECODE_OPTION_SCALING_1_2, /* 1/2 */
-    HAL_JPEG_DECODE_OPTION_SCALING_1_4, /* 1/4 */
-    HAL_JPEG_DECODE_OPTION_SCALING_1_8, /* 1/8 */
-    HAL_JPEG_DECODE_OPTION_SCALING_MAX, /* maximum number of HAL_JPEG_DECODE_OPTION_SCALING */
+    HAL_JPEG_DECODE_OPTION_SCALING_NONE = 0,    /**< none of these */
+    HAL_JPEG_DECODE_OPTION_SCALING_1_1, /**< 1/1 */
+    HAL_JPEG_DECODE_OPTION_SCALING_1_2, /**< 1/2 */
+    HAL_JPEG_DECODE_OPTION_SCALING_1_4, /**< 1/4 */
+    HAL_JPEG_DECODE_OPTION_SCALING_1_8, /**< 1/8 */
+    HAL_JPEG_DECODE_OPTION_SCALING_MAX, /**< maximum number of HAL_JPEG_DECODE_OPTION_SCALING */
 } HAL_JPEG_DECODE_OPTION_SCALING;
 
 /**
  * @brief This structure describes a rectangle specified by a point and a dimension.
  **/
 typedef struct {
-    unsigned int x; /* x coordinate of its top-left point */
-    unsigned int y; /* y coordinate of its top-left point */
-    unsigned int w; /* width of it */
-    unsigned int h; /* height of it */
+    unsigned int x; /**< x coordinate of its top-left point */
+    unsigned int y; /**< y coordinate of its top-left point */
+    unsigned int w; /**< width of it */
+    unsigned int h; /**< height of it */
 } HAL_JPEG_RECT_T;
 
 /**
  * @brief This structure describes a image information to decode or encode.
  **/
 typedef struct {
-    unsigned char *buf; /* buffer pointer of decoded raw data */
-    unsigned char *ofs_y;   /* offset of Y component */
-    unsigned char *ofs_uv;  /* offset of UV components */
-    unsigned int len_buf;   /* buffer length of decoded raw data */
-    unsigned int stride;    /* stride size of decoded raw data */
-    HAL_JPEG_RECT_T rect; /* image data rectangular */
-    HAL_JPEG_PIXEL_FORMAT pixel_format;   /* pixel format of image */
+    unsigned char *buf; /**< buffer pointer of decoded raw data */
+    unsigned char *ofs_y;   /**< offset of Y component */
+    unsigned char *ofs_uv;  /**< offset of UV components */
+    unsigned int len_buf;   /**< buffer length of decoded raw data */
+    unsigned int stride;    /**< stride size of decoded raw data */
+    HAL_JPEG_RECT_T rect; /**< image data rectangular */
+    HAL_JPEG_PIXEL_FORMAT pixel_format;   /**< pixel format of image */
 } HAL_JPEG_IMAGE_T;
 
 /**
@@ -98,31 +98,148 @@ typedef struct {
 
 /**
  * @brief Open the HW JPEG resources and set a memory to deal with decoded bit data.
- * @return if success OK, else one of error in DTV_STATUS_T.
+ *
+ * @rst
+ * Return Values
+ *   If success OK, else one of error in DTV_STATUS_T.
+ *
+ * Pseudocode
+ *   .. code-block:: cpp
+ *
+ *     DTV_STATUS_T HAL_JPEG_Open(void) { 
+ *       Open HW JPEG resources
+ *       If any eror THEN
+ *         RETURN value a one of error in DTV_STATUS_T
+ *       ELSE
+ *         RETURN API_OK
+ *       END IF
+ *     }
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_JPEG_Open();
+ *     if (ret != API_OK)
+ *       printf("fail to open HW resources");
+ *
+ * @endrst
  **/
 DTV_STATUS_T HAL_JPEG_Open(void);
 
 /**
  * @brief Decompress bit data according to the input option.
- * @param   src [in]    source image to be decompressing by HW decoder
- * @param   dst [in]    decoded destination image
- * @param   option [in]    decoding option
- * @return if success OK, else one of error in DTV_STATUS_T.
+ *
+ * @rst
+ * Parameters
+ *   - src [in]    source image to be decompressing by HW decoder
+ *   - dst [in]    decoded destination image
+ *   - option [in]    decoding option
+ *
+ * Return Values
+ *   If success OK, else one of error in DTV_STATUS_T.
+ *
+ * Pseudocode
+ *   .. code-block:: cpp
+ *
+ *     DTV_STATUS_T HAL_JPEG_Decode(src, dst, option) {
+ *       Decode source bit data
+ *       If any error THEN
+ *         RETURN value a one of error in DTV_STATUS_T
+ *       ELSE
+ *         COPY decoded raw data to the destination buffer with other data.
+ *         RETURN API_OK
+ *       END IF
+ *     }
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     HAL_JPEG_IMAGE_T src, dst;
+ *     HAL_JPEG_DECODE_OPTION_T option;
+ *     src.buf = ...;
+ *     src.len_buf = ...;
+ *     src.rect = ...;
+ *     src.image_format = ...;
+ *     src.pixel_format = ...;
+ *     option.scale = HAL_JPEG_DECODE_OPTION_SCALING_1_1;
+ *     ret = HAL_JPEG_Decode (&src, &dst, option);
+ *     if ( ret != API_OK )
+ *       printf ("fail to decode the image");
+ *
+ * @endrst
  **/
 DTV_STATUS_T HAL_JPEG_Decode(HAL_JPEG_IMAGE_T *src, HAL_JPEG_IMAGE_T *dst, HAL_JPEG_DECODE_OPTION_T *option);
 
 /**
  * @brief Open the HW JPEG resources and set a memory to deal with decoded bit data.
- * @param   src [in]    source image to be compressing by encoder
- * @param   dst [in]    encoded destination image
- * @param   option [in]    encoding option
- * @return if success OK, else one of error in DTV_STATUS_T.
+ *
+ * @rst
+ * Parameters
+ *   - src [in]    source image to be compressing by encoder
+ *   - dst [in]    encoded destination image
+ *   - option [in]    encoding option
+ *   
+ * Return Values
+ *   If success OK, else one of error in DTV_STATUS_T.
+ *
+ * Pseudocode
+ *   .. code-block:: cpp
+ *
+ *     HAL_JPEG_STATE_T HAL_JPEG_Encode(src, dst, option) {
+ *       Encode source raw data
+ *       If any error THEN
+ *         RETURN value a one of error in DTV_STATUS_T
+ *       ELSE
+ *         COPY encoded bit data to the destination buffer with other data.
+ *         RETURN API_OK
+ *       END IF
+ *     }
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     HAL_JPEG_IMAGE_T src, dst;
+ *     HAL_JPEG_ENCODE_OPTION_T option;
+ *     src.buf = ...;
+ *     src.len_buf = ...;
+ *     src.rect = ...;
+ *     src.image_format = ...;
+ *     src.pixel_format = ...;
+ *     option.quality = 100;
+ *     ret = HAL_JPEG_Encode (&src, &dst, option);
+ *     if ( ret != API_OK )
+ *     printf ("fail to encode the image");
+ *
+ * @endrst
  **/
 DTV_STATUS_T HAL_JPEG_Encode(HAL_JPEG_IMAGE_T *src, HAL_JPEG_IMAGE_T *dst, HAL_JPEG_ENCODE_OPTION_T *option);
 
 /**
  * @brief Close the HW JPEG resources and release the memory to deal with decoded bit data.
- * @return if success OK, else one of error in DTV_STATUS_T.
+ * @rst
+ * Return Values
+ *   If success OK, else one of error in DTV_STATUS_T.
+ *
+ * Pseudocode
+ *   .. code-block:: cpp
+ *
+ *     DTV_STATUS_T HAL_JPEG_Close(void) {
+ *       Close HW JPEG resources
+ *       If any eror THEN
+ *         RETURN value a one of error in DTV_STATUS_T
+ *       ELSE
+ *         RETURN API_OK
+ *       END IF
+ *     }
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_JPEG_Close();
+ *     if (ret != API_OK)
+ *       printf("fail to close HW resources");
+ *    
+ * @endrst
  **/
 DTV_STATUS_T HAL_JPEG_Close(void);
 
--- hal_inc/hal_png.h
+++ hal_inc/hal_png.h
@@ -26,89 +26,172 @@
  * This refers to https://wiki.videolan.org/YUV/.
  **/
 typedef enum {
-    HAL_PNG_PIXEL_FORMAT_NONE = 0, /* none of these */
-    HAL_PNG_PIXEL_FORMAT_PALETTE,    /* palette color type */
-    HAL_PNG_PIXEL_FORMAT_GRAYSCALE,    /* 8bit gray scale */
-    HAL_PNG_PIXEL_FORMAT_RGB,  /* 24bit RGB */
-    HAL_PNG_PIXEL_FORMAT_BGR,  /* 24bit BGR */
-    HAL_PNG_PIXEL_FORMAT_ARGB, /* 32bit ARGB */
-    HAL_PNG_PIXEL_FORMAT_ABGR, /* 32bit ABGR */
-    HAL_PNG_PIXEL_FORMAT_YUV444P,  /* planar format with each U/V values plane (YYYY UUUU VVVV) */
-    HAL_PNG_PIXEL_FORMAT_YUV444I,  /* planar format with interleaved U/V values (YYYY UVUVUVUV) */
-    HAL_PNG_PIXEL_FORMAT_YUV422P,  /* semi-planar format with each U/V values plane (2x1 subsampling ; YYYY UU VV) */
-    HAL_PNG_PIXEL_FORMAT_YUV422I,  /* semi-planar format with interleaved U/V values (2x1 subsampling ; YYYY UVUV) */
-    HAL_PNG_PIXEL_FORMAT_YUV422YUYV, /* interleaved YUV values (Y0U0Y0V0Y1U1Y1V1) for MStar Chip Vender */
-    HAL_PNG_PIXEL_FORMAT_YUV420P,  /* semi-planar format with each U/V values plane (2x2 subsampling ; YYYYYYYY UU VV) */
-    HAL_PNG_PIXEL_FORMAT_YUV420I,  /* semi-planar format with interleaved U/V values (2x2 subsampling ; YYYYYYYY UVUV) */
-    HAL_PNG_PIXEL_FORMAT_YUV400,   /* 8bit Y plane without U/V values */
-    HAL_PNG_PIXEL_FORMAT_YUV224P,  /* semi-planar format with each U/V values plane, 1 Ysamples has 2 U/V samples to horizontal (Y0Y1 U0U0U1U1V0V0V1V1) */
-    HAL_PNG_PIXEL_FORMAT_YUV224I,  /* semi-planar format with interleaved U/V values (Y0Y1 U0V0U0V0U1V1U1V1) */
-    HAL_PNG_PIXEL_FORMAT_YUV444SP,  /* sequential packed with non-planar (YUVYUVYUV...) */
-    HAL_PNG_PIXEL_FORMAT_ALPHAGRAYSCALE,    /* gray scale with alpha */
-    HAL_PNG_PIXEL_FORMAT_MAX,  /* maximum number of HAL_PNG_PIXEL_FORMAT */
+    HAL_PNG_PIXEL_FORMAT_NONE = 0, /**< none of these */
+    HAL_PNG_PIXEL_FORMAT_PALETTE,    /**< palette color type */
+    HAL_PNG_PIXEL_FORMAT_GRAYSCALE,    /**< 8bit gray scale */
+    HAL_PNG_PIXEL_FORMAT_RGB,  /**< 24bit RGB */
+    HAL_PNG_PIXEL_FORMAT_BGR,  /**< 24bit BGR */
+    HAL_PNG_PIXEL_FORMAT_ARGB, /**< 32bit ARGB */
+    HAL_PNG_PIXEL_FORMAT_ABGR, /**< 32bit ABGR */
+    HAL_PNG_PIXEL_FORMAT_YUV444P,  /**< planar format with each U/V values plane (YYYY UUUU VVVV) */
+    HAL_PNG_PIXEL_FORMAT_YUV444I,  /**< planar format with interleaved U/V values (YYYY UVUVUVUV) */
+    HAL_PNG_PIXEL_FORMAT_YUV422P,  /**< semi-planar format with each U/V values plane (2x1 subsampling ; YYYY UU VV) */
+    HAL_PNG_PIXEL_FORMAT_YUV422I,  /**< semi-planar format with interleaved U/V values (2x1 subsampling ; YYYY UVUV) */
+    HAL_PNG_PIXEL_FORMAT_YUV422YUYV, /**< interleaved YUV values (Y0U0Y0V0Y1U1Y1V1) for MStar Chip Vender */
+    HAL_PNG_PIXEL_FORMAT_YUV420P,  /**< semi-planar format with each U/V values plane (2x2 subsampling ; YYYYYYYY UU VV) */
+    HAL_PNG_PIXEL_FORMAT_YUV420I,  /**< semi-planar format with interleaved U/V values (2x2 subsampling ; YYYYYYYY UVUV) */
+    HAL_PNG_PIXEL_FORMAT_YUV400,   /**< 8bit Y plane without U/V values */
+    HAL_PNG_PIXEL_FORMAT_YUV224P,  /**< semi-planar format with each U/V values plane, 1 Ysamples has 2 U/V samples to horizontal (Y0Y1 U0U0U1U1V0V0V1V1) */
+    HAL_PNG_PIXEL_FORMAT_YUV224I,  /**< semi-planar format with interleaved U/V values (Y0Y1 U0V0U0V0U1V1U1V1) */
+    HAL_PNG_PIXEL_FORMAT_YUV444SP,  /**< sequential packed with non-planar (YUVYUVYUV...) */
+    HAL_PNG_PIXEL_FORMAT_ALPHAGRAYSCALE,    /**< gray scale with alpha */
+    HAL_PNG_PIXEL_FORMAT_MAX,  /**< maximum number of HAL_PNG_PIXEL_FORMAT */
 } HAL_PNG_PIXEL_FORMAT;
 
 /**
  * @brief This enumeration describes down-sampling factor using HW decoder.
  **/
 typedef enum {
-    HAL_PNG_DECODE_OPTION_SCALING_NONE = 0, /* none of these */
-    HAL_PNG_DECODE_OPTION_SCALING_1_1,  /* 1/1 */
-    HAL_PNG_DECODE_OPTION_SCALING_1_2,  /* 1/2 */
-    HAL_PNG_DECODE_OPTION_SCALING_1_4,  /* 1/4 */
-    HAL_PNG_DECODE_OPTION_SCALING_1_8,  /* 1/8 */
-    HAL_PNG_DECODE_OPTION_SCALING_MAX,  /* maximum number of HAL_PNG_DECODE_OPTION_SCALING */
+    HAL_PNG_DECODE_OPTION_SCALING_NONE = 0, /**< none of these */
+    HAL_PNG_DECODE_OPTION_SCALING_1_1,  /**< 1/1 */
+    HAL_PNG_DECODE_OPTION_SCALING_1_2,  /**< 1/2 */
+    HAL_PNG_DECODE_OPTION_SCALING_1_4,  /**< 1/4 */
+    HAL_PNG_DECODE_OPTION_SCALING_1_8,  /**< 1/8 */
+    HAL_PNG_DECODE_OPTION_SCALING_MAX,  /**< maximum number of HAL_PNG_DECODE_OPTION_SCALING */
 } HAL_PNG_DECODE_OPTION_SCALING;
 
 /**
  * @brief This structure describes a rectangle specified by a point and a dimension.
  **/
 typedef struct {
-    unsigned int x; /* x coordinate of its top-left point */
-    unsigned int y; /* y coordinate of its top-left point */
-    unsigned int w; /* width of it */
-    unsigned int h; /* height of it */
+    unsigned int x; /**< x coordinate of its top-left point */
+    unsigned int y; /**< y coordinate of its top-left point */
+    unsigned int w; /**< width of it */
+    unsigned int h; /**< height of it */
 } HAL_PNG_RECT_T;
 
 /**
  * @brief This structure describes a image information to decode or encode.
  **/
 typedef struct {
-    unsigned char *buf; /* buffer pointer of decoded raw data */
-    unsigned char *ofs_y;   /* offset of Y component */
-    unsigned char *ofs_uv;  /* offset of UV components */
-    unsigned int len_buf;   /* buffer length of decoded raw data */
-    unsigned int stride;    /* stride size of decoded raw data */
-    HAL_PNG_RECT_T rect; /* image data rectangular */
-    HAL_PNG_PIXEL_FORMAT pixel_format;   /* pixel format of image */
+    unsigned char *buf; /**< buffer pointer of decoded raw data */
+    unsigned char *ofs_y;   /**< offset of Y component */
+    unsigned char *ofs_uv;  /**< offset of UV components */
+    unsigned int len_buf;   /**< buffer length of decoded raw data */
+    unsigned int stride;    /**< stride size of decoded raw data */
+    HAL_PNG_RECT_T rect; /**< image data rectangular */
+    HAL_PNG_PIXEL_FORMAT pixel_format;   /**< pixel format of image */
 } HAL_PNG_IMAGE_T;
 
 /**
  * @brief This struct describes option to decode image.
  **/
 typedef struct {
-    HAL_PNG_DECODE_OPTION_SCALING scale;    /* decoding scale option */
+    HAL_PNG_DECODE_OPTION_SCALING scale;    /**< decoding scale option */
 } HAL_PNG_DECODE_OPTION_T;
 
 
 /**
  * @brief Open the HW PNG resources and set a memory to deal with decoded bit data.
- * @return if success OK, else one of error in DTV_STATUS_T.
+ *
+ * @rst
+ * Return Values
+ *   If success OK, else one of error in DTV_STATUS_T.
+ *
+ * Pseudocode
+ *   .. code-block:: cpp
+ *
+ *     DTV_STATUS_T HAL_PNG_Open(void) {
+ *       Open HW PNG resources
+ *       If any eror THEN
+ *         RETURN value a one of error in DTV_STATUS_T
+ *       ELSE
+ *         RETURN API_OK
+ *       END IF
+ *     }
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_PNG_Open();
+ *     if (ret != API_OK)
+ *       printf("fail to open HW resources");
+ *
+ * @endrst
  **/
 DTV_STATUS_T HAL_PNG_Open(void);
 
 /**
  * @brief Decompress bit data according to the input option.
- * @param   src [in]    source image to be decompressing by HW decoder
- * @param   dst [in]    decoded destination image
- * @param   option [in]    decoding option
- * @return if success OK, else one of error in DTV_STATUS_T.
+ *
+ * @rst
+ * Parameters
+ *   - src [in]    source image to be decompressing by HW decoder
+ *   - dst [in]    decoded destination image
+ *   - option [in]    decoding option
+ *
+ * Return Values
+ *   if success OK, else one of error in DTV_STATUS_T.
+ *
+ * Pseudocode
+ *   .. code-block:: cpp
+ *
+ *     DTV_STATUS_T HAL_PNG_Decode(src, dst, option) {
+ *       Decode source bit data
+ *       If any error THEN
+ *         RETURN value a one of error in DTV_STATUS_T
+ *       ELSE
+ *         COPY decoded raw data to the destination buffer with other data.
+ *         RETURN API_OK
+ *       END IF
+ *     }
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     HAL_PNG_IMAGE_T src, dst;
+ *     HAL_PNG_DECODE_OPTION_T option;
+ *     src.buf = ...;
+ *     src.len_buf = ...;
+ *     src.rect = ...;
+ *     src.image_format = ...;
+ *     src.pixel_format = ...;
+ *     option.scale = HAL_PNG_DECODE_OPTION_SCALING_1_1;
+ *     ret = HAL_PNG_Decode (&src, &dst, option);
+ *     if ( ret != API_OK )
+ *       printf ("fail to decode the image");
+ *
+ * @endrst
  **/
 DTV_STATUS_T HAL_PNG_Decode(HAL_PNG_IMAGE_T *src, HAL_PNG_IMAGE_T *dst, HAL_PNG_DECODE_OPTION_T *option);
 
 /**
  * @brief Close the HW PNG resources and release the memory to deal with decoded bit data.
- * @return if success OK, else one of error in DTV_STATUS_T.
+ *
+ * @rst
+ * Return Values
+ *   if success OK, else one of error in DTV_STATUS_T.
+ *
+ * Pseudocode
+ *   .. code-block:: cpp
+ *
+ *     DTV_STATUS_T HAL_PNG_Close(void) {
+ *       Close HW PNG resources
+ *       If any eror THEN
+ *         RETURN value a one of error in DTV_STATUS_T
+ *       ELSE
+ *         RETURN API_OK
+ *       END IF
+ *     }
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_PNG_Close();
+ *     if (ret != API_OK)
+ *     printf("fail to close HW resources");
+ *
+ * @endrst
  **/
 DTV_STATUS_T HAL_PNG_Close(void);
 
