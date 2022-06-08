aba9128428d8ac4d3346995a37088341f4d2fef2
denis.hong <denis.hong@lge.com>
2022-06-08 17:28:55 +0900

    Add system socts api(mmc)
    :Release Notes:
    Add system socts api(mmc)
    
    :Detailed Notes:
    Added API for controlling mmc
    Target Branch :master
    Request & agreed with all SoC Vendor : [yes]
    Update linuxtv-ext Interface I/G : [yes]
    
    :Testing Performed:
    build : ok
    generate documentation : ok
    
    :Issues Addressed:
    [TVPLAT-212460]
    
    Change-Id: If3b137fe0bcccef09f9df9d4cacae9579ff01a59

--- hal_inc/hal_mmc.h
+++ hal_inc/hal_mmc.h
@@ -56,13 +56,254 @@ typedef enum {
 /******************************************************************************
         Function Declaration
 ******************************************************************************/
+/**
+ * @brief Open MMC device
+ *
+ * @rst
+ * Functional Requirements
+ *   There are no functional requirements.
+ *
+ * Responses to abnormal situations, including
+ *   In abnormal case, the BSP should return -1.
+ *
+ * Performance Requirements
+ *   There are no performance requirements.
+ *
+ * Constraints
+ *   There must be symmetric key exchanged with Apple on the board.
+ *   If there is no symmetric key, this function cannot be tested.
+ *
+ * Functions & Parameters
+ *   .. code-block:: cpp
+ *
+ *    DTV_STATUS HAL_MMC_Open(void);
+ *
+ * Return Value
+ *   Success: 0
+ *   Fail: -1
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_MMC_Open();
+ *
+ * @endrst
+ */
 DTV_STATUS_T HAL_MMC_Open(void);
-DTV_STATUS_T HAL_MMC_Close(void);
-DTV_STATUS_T HAL_MMC_PON_Short(void);
-DTV_STATUS_T HAL_MMC_PON_Long(void);
-DTV_STATUS_T HAL_MMC_Get_CID(unsigned char *buff);
-DTV_STATUS_T HAL_MMC_Get_SMART_Report(unsigned char *buff, unsigned int *len);
-DTV_STATUS_T HAL_MMC_Get_Register(MMC_REGISTER_TYPE_T mmcRegisterType, unsigned char* buff);
 
+/**
+ * @brief Close MMC device
+ *
+ * @rst
+ * Functional Requirements
+ *   There are no functional requirements
+ *
+ * Responses to abnormal situations, including
+ *   In abnormal case, the BSP should return -1.
+ *
+ * Performance Requirements
+ *   There are no performance requirements.
+ *
+ * Constraints
+ *   There must be symmetric key exchanged with Apple on the board.
+ *   If there is no symmetric key, this function cannot be tested.
+ *
+ * Functions & Parameters
+ *   .. code-block:: cpp
+ *
+ *    DTV_STATUS HAL_MMC_Close(void);
+ *
+ * Return Value
+ *   Success: 0
+ *   Fail: -1
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_MMC_Close();
+ *
+ * @endrst
+ */
+DTV_STATUS_T HAL_MMC_Close(void);
+
+/**
+ * @brief Set PON(Power Off Notification) with short time
+ *
+ * @rst
+ * Functional Requirements
+ *   There are no functional requirements
+ *
+ * Responses to abnormal situations, including
+ *   In abnormal case, the BSP should return -1.
+ *
+ * Performance Requirements
+ *   There are no performance requirements.
+ *
+ * Constraints
+ *   There must be symmetric key exchanged with Apple on the board.
+ *   If there is no symmetric key, this function cannot be tested.
+ *
+ * Functions & Parameters
+ *  .. code-block:: cpp
+ *
+ *    DTV_STATUS HAL_MMC_PON_Short();
+ *
+ * Return Value
+ *   Success: 0
+ *   Fail: -1
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_MMC_PON_Short();
+ *
+ * @endrst
+ */
+DTV_STATUS_T HAL_MMC_PON_Short(void);
+
+/**
+ * @brief Set PON(Power Off Notification) with long time
+ *
+ * @rst
+ * Functional Requirements
+ *   There are no functional requirements
+ *
+ * Responses to abnormal situations, including
+ *   In abnormal case, the BSP should return -1.
+ *
+ * Performance Requirements
+ *   There are no performance requirements.
+ *
+ * Constraints
+ *   There must be symmetric key exchanged with Apple on the board.
+ *   If there is no symmetric key, this function cannot be tested.
+ *
+ * Functions & Parameters
+ *   .. code-block:: cpp
+ *
+ *    DTV_STATUS HAL_MMC_PON_Long();
+ *
+ * Return Value
+ *   Success: 0
+ *   Fail: -1
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     ret = HAL_MMC_PON_Long();
+ *
+ * @endrst
+ */
+DTV_STATUS_T HAL_MMC_PON_Long(void);
+
+/**
+ * @brief Get MMC CID(Card IDentification)
+ *
+ * @rst
+ * Functional Requirements
+ *   There are no functional requirements
+ *
+ * Responses to abnormal situations, including
+ *   In abnormal case, the BSP should return -1.
+ *
+ * Performance Requirements
+ *   There are no performance requirements.
+ *
+ * Constraints
+ *   There must be symmetric key exchanged with Apple on the board.
+ *   If there is no symmetric key, this function cannot be tested.
+ *
+ * Functions & Parameters
+ *   .. code-block:: cpp
+ *
+ *    DTV_STATUS HAL_MMC_Get_CID(unsigned char *buff);
+ *
+ * Return Value
+ *   Success: 0
+ *   Fail: -1
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     unsigned char* buff;
+ *     ret =  HAL_MMC_Get_CID(buff);
+ *
+ * @endrst
+ */
+DTV_STATUS_T HAL_MMC_Get_CID(unsigned char *buff);
+
+/**
+ * @brief Get MMC usage information. (SMART : Self Monitoring, Analysis and Reporting Technology)
+ *
+ * @rst
+ * Functional Requirements
+ *   There are no functional requirements
+ *
+ * Responses to abnormal situations, including
+ *   In abnormal case, the BSP should return -1.
+ *
+ * Performance Requirements
+ *   There are no performance requirements.
+ *
+ * Constraints
+ *   There must be symmetric key exchanged with Apple on the board.
+ *   If there is no symmetric key, this function cannot be tested.
+ *
+ * Functions & Parameters
+ *   .. code-block:: cpp
+ *
+ *    DTV_STATUS HAL_MMC_Get_SMART_Report(unsigned char *buff, unsigned int *len);
+ *
+ * Return Value
+ *   Success: 0
+ *   Fail: -1
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     unsigned char* buff;
+ *     unsigned int *len;
+ *     ret =  HA HAL_MMC_Get_SMART_Report(buff, len);
+ *
+ * @endrst
+ */
+DTV_STATUS_T HAL_MMC_Get_SMART_Report(unsigned char *buff, unsigned int *len);
+
+/**
+ * @brief Get MMC register's information.
+ *
+ * @rst
+ * Functional Requirements
+ *   There are no functional requirements
+ *
+ * Responses to abnormal situations, including
+ *   In abnormal case, the BSP should return -1.
+ *
+ * Performance Requirements
+ *   There are no performance requirements.
+ *
+ * Constraints
+ *   There must be symmetric key exchanged with Apple on the board.
+ *   If there is no symmetric key, this function cannot be tested.
+ *
+ * Functions & Parameters
+ *   .. code-block:: cpp
+ *
+ *    DTV_STATUS HAL_MMC_Get_Register(MMC_REGISTER_TYPE_T mmcRegisterType, unsigned char *buff);
+ *
+ * Return Value
+ *   Success: 0
+ *   Fail: -1
+ *
+ * Example
+ *   .. code-block:: cpp
+ *
+ *     unsigned char* buff;
+ *     MMC_REGISTER_TYPE_T mmcRegisterType;
+ *     ret =  HAL_MMC_Get_Register(mmcRegisterType, buff);
+ *
+ * @endrst
+ */
+DTV_STATUS_T HAL_MMC_Get_Register(MMC_REGISTER_TYPE_T mmcRegisterType, unsigned char *buff);
 #endif  /* _hal_mmc.h_ */
 
