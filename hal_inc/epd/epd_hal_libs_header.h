2d7969b131f73bec87d2c57aa2a01a4880ce2855
kyungna.park <kyungna.park@lge.com>
2025-08-07 13:21:01 +0900

    Add epd header
    :Release Notes:
    Add epd header
    
    :Detailed Notes:
    - Make a new directory: 'epd'
    - Make header for 'epd' directory, includes required headers:
      'epd_hal_libs_header.h'
    
    :Testing Performed:
    Test: OK
    
    :Issues Addressed:
    [TVPLAT-691956]
    
    Change-Id: Icc12467437263d7fc048c6a4c35c785daebd4d5b

---
+++ hal_inc/epd/epd_hal_libs_header.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *   DTV LABORATORY, LG ELECTRONICS INC., SEOUL, KOREA
+ *   Copyright(c) 2025 by LG Electronics Inc.
+ *
+ *   All rights reserved. No part of this work may be reproduced, stored in a
+ *   retrieval system, or transmitted by any means without prior written
+ *   permission of LG Electronics Inc.
+
+ *****************************************************************************/
+
+ #ifndef _HAL_LIBS_HEADER_H_
+ #define _HAL_LIBS_HEADER_H_
+
+ #include "hal_common.h"
+ #include "hal_mmc.h"
+ #include "hal_sys.h"
+ #include "hal_ucom.h"
+ #include "hal_usb.h"
+ #include "hal-ver.h"
+
+ #endif
